ce3da4fa482813f17965058cff5a63b96039a9a7
roy-santos <roysantos112@gmail.com>
2019-12-19 20:24:20 -0800

    Fill out model classes. Inventory class still needs work.

--- src/Model/InHouse.java
+++ src/Model/InHouse.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class InHouse {
+public class InHouse extends Part {
+
+    private int machineId;
+
+
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+    }
 }
--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -1,10 +1,63 @@
 package Model;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Inventory {
 
-    private static ObservableList allParts;
-    private static ObservableList allProducts;
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    public static void addPart(Part part) {
+
+        allParts.add(part);
+
+    }
+
+    public static void addProduct(Product product) {
+
+        allProducts.add(product);
+
+    }
+
+    public static Part lookupPart(int partId) {
+
+    }
+
+    public static Product lookupProduct(int productId) {
+
+    }
+
+    public static Part lookupPart(String partName) {
+
+    }
+
+    public static Product lookupProduct(String productName) {
+
+    }
+
+    public static void updatePart(int index, Part part) {
+        // refer back to int index parameter on UML diagram
+    }
+
+    public static void updateProduct(int index, Product product) {
+        // refer back to int index parameter on UML diagram
+    }
+
+    public static void deletePart(Part part) {
+
+    }
+
+    public static void deleteProduct(Product product) {
+
+    }
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
+    }
 
 }
